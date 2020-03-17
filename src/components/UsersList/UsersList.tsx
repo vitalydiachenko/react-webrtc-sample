34ec7a673bfe958f1886a43d2beadc8bb241e9f3
Vitaly Diachenko <v.di4chenko@gmail.com>
2020-03-17 17:17:08 +0200

    [feature] Add UserList component within VideoCall

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,44 @@
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import { default as AccountBoxIcon } from '@material-ui/icons/AccountBox';
+import * as React from 'react';
+import './UsersList.less';
+
+interface IUsersListProps {
+  users: string[];
+}
+
+function UsersList(props: IUsersListProps) {
+  const { users } = props;
+
+  return (
+    <div className="UsersList">
+      <div className="UsersList__Header">
+        <Typography variant="h5" color="textPrimary">
+          Contacts
+        </Typography>
+      </div>
+      <div className="UsersList__Main">
+        {!users.length ? (
+          <Typography variant="overline">Nobody is connected now!</Typography>
+        ) : (
+          <List>
+            {users.map(user => (
+              <ListItem key={`USERS_LIST_${user}`}>
+                <ListItemIcon>
+                  <AccountBoxIcon />
+                </ListItemIcon>
+                <ListItemText>User #{user}</ListItemText>.
+              </ListItem>
+            ))}
+          </List>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default React.memo(UsersList);
--- src/containers/VideoCall/VideoCall.tsx
+++ src/containers/VideoCall/VideoCall.tsx
@@ -1,29 +1,47 @@
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import UsersList from 'components/UsersList';
 import * as React from 'react';
+import { useState } from 'react';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   callContainer: {
     padding: '16px',
   },
   main: {
-    margin: '16px 0',
+    margin: '32px 0',
+  },
+  sideBar: {
+    [theme.breakpoints.only('xs')]: {
+      margin: '0 0 24px',
+    },
   },
   titles: {
-    margin: '32px 0 16px',
+    margin: '64px 0 32px',
   },
-});
+}));
 
 function VideoCall() {
+  const [isUsersListOpen, toggleUsersListVisibility] = useState<boolean>(false);
+
+  const handleUsersListClose = () => {
+    toggleUsersListVisibility(false);
+  };
+
+  const handleUsersListOpen = () => {
+    toggleUsersListVisibility(true);
+  };
+
   const classes = useStyles();
 
   return (
     <div>
-      <Grid container={true} spacing={2}>
+      <Grid container={true}>
         <Grid className={classes.titles} item={true} xs={12}>
           <Typography align="left" variant="h1" color="textPrimary">
             Video Call
@@ -35,17 +53,20 @@ function VideoCall() {
       </Grid>
       <Divider />
       <Grid container={true} className={classes.main}>
-        <Grid item={true} xs={12} md={4} lg={2}>
-          <Button variant="contained" color="primary">
+        <Grid item={true} className={classes.sideBar} xs={12} md={2}>
+          <Button variant="contained" color="primary" onClick={handleUsersListOpen}>
             Open Contacts
           </Button>
         </Grid>
-        <Grid item={true} xs={12} md={8} lg={10}>
+        <Grid item={true} xs={12} md={10}>
           <Paper className={classes.callContainer} elevation={3}>
             Video Call Block
           </Paper>
         </Grid>
       </Grid>
+      <Drawer open={isUsersListOpen} anchor="left" onClose={handleUsersListClose}>
+        <UsersList users={[]} />
+      </Drawer>
     </div>
   );
 }
