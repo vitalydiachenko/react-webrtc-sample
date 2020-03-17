c9731e6330d520cd3ffd217434a6a6bec48c7242
Vitaly Diachenko <v.di4chenko@gmail.com>
2020-03-17 18:32:22 +0200

    [feature] Add VideoPreview component

---
+++ src/containers/VideoPreview/VideoPreview.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './VideoPreview.less';
+
+function VideoPreview() {
+  return (
+    <div className="VideoPreview">
+      <video />
+    </div>
+  );
+}
+
+export default VideoPreview;
